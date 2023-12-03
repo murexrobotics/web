50860af7d8bde0ee1b1f8e6e8319162f55286e2b
Hello9999901 <byran8855@gmail.com>
2023-12-03 00:21:50 -0500

    Feat[compression]: added astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-
 import svelte from "@astrojs/svelte";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.murexrobotics.com",
-  integrations: [tailwind(), react(), partytown(), prefetch(), sitemap(), svelte()],
+  integrations: [tailwind(), react(), partytown(), prefetch(), sitemap(), svelte(), compress()]
 });
